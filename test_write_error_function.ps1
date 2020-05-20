256213992462a1fe57aada62ff0074ee6e46ee78
Ben Weedon <ben@weedon.email>
2020-05-19 20:14:18 -0700

    Don't need New-Module

--- test_write_error_function.ps1
+++ test_write_error_function.ps1
@@ -1,33 +1,78 @@
 Import-Module -Force $PSScriptRoot\docgen
 
 OutputText @'
-When $ErrorActionPreference is "Stop", $PSCmdlet.WriteError exits the current
-advanced function and throws. However, the exception it throws is not catchable
-from within the advanced function.
+When <code>$ErrorActionPreference</code> is <code>Stop</code>,
+<code>$PSCmdlet.WriteError</code> exits the current advanced function and
+throws. However, the exception it throws is not catchable from within the
+advanced function.
 '@
 
 OutputCode {
-    New-Module {
-        Import-Module -Force $PSScriptRoot\util
+    function TestWriteErrorFunction {
+        [CmdletBinding()]
+        param()
 
-        function TestWriteErrorFunction {
-            [CmdletBinding()]
-            param()
-
-            $local:ErrorActionPreference = "Stop"
-            try {
-                $PSCmdlet.WriteError((NewErrorRecord "error in try"))
-            } catch {
-                Write-Output "caught inside the cmdlet: $_"
-            }
-
-            Write-Output "after the try-catch"
+        $local:ErrorActionPreference = "Stop"
+        try {
+            $PSCmdlet.WriteError((NewErrorRecord "error in try"))
+        } catch {
+            Write-Output "caught inside the cmdlet: $_"
         }
-        Export-ModuleMember -Function TestWriteErrorFunction
-    } > $null
+
+        Write-Output "after the try-catch"
+    }
 
     try {
         TestWriteErrorFunction
+    } catch {
+        Write-Output "caught outside the cmdlet: $_"
+    }
+}
+
+OutputText @'
+Write-Error, on the other hand, is catchable inside the function, for both
+advanced and basic functions.
+'@
+
+OutputCode {
+    function TestWriteErrorCmdletAdvanced {
+        [CmdletBinding()]
+        param()
+
+        $local:ErrorActionPreference = "Stop"
+        try {
+            Write-Error "error in try"
+        } catch {
+            Write-Output "caught inside the cmdlet: $_"
+        }
+
+        Write-Output "after the try-catch"
+    }
+
+    function TestWriteErrorCmdletBasic {
+        [CmdletBinding()]
+        param()
+
+        $local:ErrorActionPreference = "Stop"
+        try {
+            Write-Error "error in try"
+        } catch {
+            Write-Output "caught inside the cmdlet: $_"
+        }
+
+        Write-Output "after the try-catch"
+    }
+
+    Write-Output "testing advanced function"
+    try {
+        TestWriteErrorCmdletAdvanced
+    } catch {
+        Write-Output "caught outside the cmdlet: $_"
+    }
+
+    Write-Output "testing basic function"
+    try {
+        TestWriteErrorCmdletBasic
     } catch {
         Write-Output "caught outside the cmdlet: $_"
     }
