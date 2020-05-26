bb318f9760467073ad80cad36502e40041b340f0
Ben Weedon <ben@weedon.email>
2020-05-25 18:53:11 -0700

    Use the viewport meta tag to improve scaling on mobile devices

--- generateHtml.ps1
+++ generateHtml.ps1
@@ -28,6 +28,7 @@ $htmlPrefix = @"
 <html lang="en-US">
     <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>PowerShell live documentation</title>
         <link rel="stylesheet" type="text/css" href="style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/styles/default.min.css">
