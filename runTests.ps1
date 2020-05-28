9d97e8f734c23e6c193d0342d22aed154532c31c
Ben Weedon <ben@weedon.email>
2020-05-27 19:30:45 -0700

    Upgrade pester to version 5
    Our CI started failing because it was installing the latest version of
    pester, which is now 5.0 and has breaking changes from 4.x. Basically we
    need to use a PesterConfiguration object rather than just passing in the
    -EnableExit parameter.

--- runTests.ps1
+++ runTests.ps1
@@ -3,7 +3,15 @@ try {
     # Invoke-Pester needs to run in its own powershell session because
     # otherwise classes in modules like docgen get cached and can't be changed
     # during development. Import-Module -Force doesn't work with classes :(
-    pwsh -c Invoke-Pester -EnableExit
+    pwsh -c {
+        Import-Module pester -Force
+        $config = [PesterConfiguration]::Default
+
+        # Exit with non-zero exit code when the test run fails.
+        $config.Run.Exit = $true
+
+        Invoke-Pester -Configuration $config
+    }
     $exitCode = $LASTEXITCODE
 } finally {
     Pop-Location
