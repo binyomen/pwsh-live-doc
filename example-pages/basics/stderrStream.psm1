3ab75004e6b3a4196891bbe995ac1cc4469735a1
Ben Weedon <ben@weedon.email>
2020-07-29 00:32:45 -0700

    Improve page formatting and wording

--- example-pages/basics/stderrStream.psm1
+++ example-pages/basics/stderrStream.psm1
@@ -20,17 +20,19 @@ function RunPage {
     OutputSection 'Basic case' {
         OutputText @'
         When `ErrorActionPreference` is `Continue`, stderr in PowerShell Core
-        behaves similarly to stderr in other shells. Any stderr output by
+        behaves similarly to stderr in other shells. Most stderr output by
         commands you call will be sent directly to the PowerShell process's own
         stderr. It is interesting to note, however, that assigning the stderr
-        command a variable, casting it to `Void`, or piping it to `Out-Null`
+        command to a variable, casting it to `Void`, or piping it to `Out-Null`
         results in the stderr stream being received by stdout.
 
-        In PowerShell 2 and 5, the result is much different. Not only
-        are stack traces printed out to stderr (except for the first print),
-        nothing is printed to stdout. Everything is printed to stderr except
-        redirecting to `$null` or a file and the redirect version of the
+        In PowerShell 2 and 5, the result is much different. Not only are stack
+        traces printed out to stderr (except for the first print for some
+        reason), nothing is printed to stdout. Everything is printed to stderr
+        except redirecting to `$null` or a file and the redirect version of the
         variable, `Void`, and `Out-Null` cases.
+
+        Here is the output from writing directly to stderr:
 '@
 
         OutputCode {
@@ -38,9 +40,29 @@ function RunPage {
 
             cmd /c 'echo printing to stderr 1 >&2'
             cmd /c 'echo printing to stderr 2 >&2'
+        }
+
+        OutputText @'
+        Here we are redirecting to stdout, `$null`, and a file:
+'@
+
+        OutputCode {
+            $local:ErrorActionPreference = 'Continue'
+
             cmd /c 'echo redirecting stderr to stdout >&2' 2>&1
             cmd /c 'echo redirecting stderr to $null >&2' 2> $null
             cmd /c 'echo redirecting stderr to a file >&2' 2> error.txt
+            Write-Output "error.txt: $((Get-Content error.txt) -join `"`n`")"
+        }
+
+        OutputText @'
+        And finally here we redirect to a variable, cast to `Void`, and
+        redirect to `Out-Null`:
+'@
+
+        OutputCode {
+            $local:ErrorActionPreference = 'Continue'
+
             $v = cmd /c 'echo assigning command result to a variable >&2'
             Write-Output "`$v: $v"
             $v = cmd /c 'echo assigning redirected command result to a variable >&2' 2>&1
@@ -49,13 +71,13 @@ function RunPage {
             [Void] (cmd /c 'echo casting redirected stderr command to Void >&2' 2>&1)
             cmd /c 'echo piping stderr command to Out-Null >&2' | Out-Null
             cmd /c 'echo piping redirected stderr command to Out-Null >&2' 2>&1 | Out-Null
-
-            Write-Output "error.txt: $((Get-Content error.txt) -join `"`n`")"
         }
 
         OutputText @'
         If you redirect stderr while `ErrorActionPreference` is `Stop`, an
-        exception is generated.
+        exception is generated. Here we have the same groupings, first writing
+        directly to stderr. Since we aren't redirecting stderr, no exceptions
+        are thrown:
 '@
 
         OutputCode {
@@ -66,60 +88,87 @@ function RunPage {
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 cmd /c 'echo printing to stderr 2 >&2'
             } catch {
                 Write-Output "Caught: $_"
             }
+        }
+
+        OutputText @'
+        Then redirecting to stdout, `$null`, or a file all cause exceptions:
+'@
+
+        OutputCode {
+            $local:ErrorActionPreference = 'Stop'
+
             try {
                 cmd /c 'echo redirecting stderr to stdout >&2' 2>&1
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 cmd /c 'echo redirecting stderr to $null >&2' 2> $null
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 cmd /c 'echo redirecting stderr to a file >&2' 2> error.txt
             } catch {
                 Write-Output "Caught: $_"
             }
+
+            Write-Output "error.txt: $((Get-Content error.txt) -join `"`n`")"
+        }
+
+        OutputText @'
+        And finally redirecting to a variable, casting to `Void`, or
+        redirecting to `Out-Null` ??????:
+'@
+
+        OutputCode {
+            $local:ErrorActionPreference = 'Stop'
+
             try {
                 $v = cmd /c 'echo assigning command result to a variable >&2'
                 Write-Output "`$v: $v"
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 $v = cmd /c 'echo assigning redirected command result to a variable >&2' 2>&1
                 Write-Output "`$v: $v"
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 [Void] (cmd /c 'echo casting stderr command to Void >&2')
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 [Void] (cmd /c 'echo casting redirected stderr command to Void >&2' 2>&1)
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 cmd /c 'echo piping stderr command to Out-Null >&2' | Out-Null
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 cmd /c 'echo piping redirected stderr command to Out-Null >&2' 2>&1 | Out-Null
             } catch {
                 Write-Output "Caught: $_"
             }
-
-            Write-Output "error.txt: $((Get-Content error.txt) -join `"`n`")"
         }
     }
 
@@ -158,6 +207,7 @@ function RunPage {
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 $c.VoidFunc()
             } catch {
@@ -195,6 +245,7 @@ function RunPage {
             } catch {
                 Write-Output "Caught: $_"
             }
+
             try {
                 $c.VoidFunc()
             } catch {
